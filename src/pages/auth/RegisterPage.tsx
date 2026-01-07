65a8b35b93f2a8938c8fc4120e53e1101b4668c9
Pranjal Thapa <pranjal.thapa555@gmail.com>
2026-01-07 20:30:42 +0545

    rest code

---
+++ src/pages/auth/RegisterPage.tsx
@@ -0,0 +1,11 @@
+//import RegisterForm from "../../components/auth/RegisterForm";
+//import { PageHeadingWithSubtitle } from "../../components/page-heading/PageHeading";
+export default function RegisterPage() {
+
+    return (
+        <>
+                    Register Form 
+
+        </>
+    );
+}
