13d6462219a65a64b85e8ce921cb78bce010cef8
Pranjal Thapa <pranjal.thapa555@gmail.com>
2026-01-09 12:00:14 +0545

    user router

--- src/pages/layout/AuthLayout.tsx
+++ src/pages/layout/AuthLayout.tsx
@@ -1,37 +1,56 @@
-import { NavLink, Outlet } from "react-router"
-import { PageHeadingWithSubtitle } from "../../components/page-heading/PageHeading"
+import {useState} from "react";
+import { NavLink, Outlet } from "react-router";
+import { PageHeadingWithSubtitle } from "../../components/page-heading/PageHeading";
+import { type Dispatch, type SetStateAction} from "react"
+import { type IOutletContextProps } from "../auth/LoginPage";
+
+export interface ILayoutData{
+  pageTitle: string,
+  subTitle:string,
+  btnTxt:string,
+  btnLink: string
+}
+export interface IOutletContext{
+  setLayoutData: Dispatch<SetStateAction<IOutletContextProps>>
+}
 export default function AuthLayout() {
-    return (<>
-        <section className="h-screen flex flex-col items-center justify-center p-2">
-            <div className="w-full md:w-2xl lg:w-3xl xl:w-4xl shadow-2xl bg-stone-200/75 rounded-tl-lg rounded-br-lg p-4  py-10 ">
+  const [ layoutData, setLayoutData] = useState<ILayoutData>({
+    pageTitle: "Login from Here",
+    subTitle: "Lorem ipsum yestai ho",
+    btnTxt: "Register Here",
+    btnLink: "/register"
+  })
+  return (
+    <>
+      <section className="h-screen flex flex-col items-center justify-center p-2">
+        <div className="w-full md:w-2xl lg:w-3xl xl:w-4xl shadow-2xl bg-stone-200/75 rounded-tl-lg rounded-br-lg p-4  py-10 ">
+          <PageHeadingWithSubtitle title={layoutData.pageTitle}>
+           {layoutData.subTitle}
+          </PageHeadingWithSubtitle>
 
-                <PageHeadingWithSubtitle title="Login From here">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </PageHeadingWithSubtitle>
+          <Outlet  context={{
+            layoutData,
+            setLayoutData
+          }}/>
+          <div className="w-full flex flex-col gap-5">
+            <span className="flex items-center">
+              <span className="h-px flex-1 bg-gray-300"></span>
+              <span className="shrink-0 px-4 text-gray-900">OR</span>
 
-              <Outlet/>
+              <span className="h-px flex-1 bg-gray-300"></span>
+            </span>
 
-
-                <div className="w-full flex flex-col gap-5">
-                    <span className="flex items-center">
-                        <span className="h-px flex-1 bg-gray-300"></span>
-                        <span className="shrink-0 px-4 text-gray-900">OR</span>
-
-                        <span className="h-px flex-1 bg-gray-300"></span>
-                    </span>
-
-
-
-                    <div className="flex w-full justify-center">
-                        
-                        <NavLink to="/register"
-                         className="cursor-pointer hover:underline  transition hover:scale-96 w-full p-2 rounded-full border border-emerald-800 text-green-950 text-lg text-center">
-                            Register here
-                        </NavLink>
-                    </div>
-
-                </div>
+            <div className="flex w-full justify-center">
+              <NavLink
+                to={layoutData.btnLink}
+                className="cursor-pointer hover:underline  transition hover:scale-96 w-full p-2 rounded-full border border-emerald-800 text-green-950 text-lg text-center"
+              >
+               {layoutData.btnTxt}
+              </NavLink>
             </div>
-        </section>
-    </>)
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/router/RouterConfig.tsx
+++ src/router/RouterConfig.tsx
@@ -6,30 +6,53 @@ import ErrorPage from "../pages/ErrorPage";
 import AuthLayout from "../pages/layout/AuthLayout";
 import UserLayout from "../pages/layout/UserLayout";
 import UserDashboard from "../pages/dashboard/UserDashboard";
+import UserListPage from "../pages/user/UserListPage";
 
-
-const router=createBrowserRouter([
-   // {path:"/",Component:LoginPage}
-   //{path:"/",element:<LoginPage/>},
-   {path:"/",element:<AuthLayout/>,children:[{
-      index:true,element:<LoginPage/>},
-   {path:"register",Component:RegisterPage}
-]
-},
+const router = createBrowserRouter([
+  // {path:"/",Component:LoginPage}
+  //{path:"/",element:<LoginPage/>},
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        element: <LoginPage />,
+      },
+      { path: "register", Component: RegisterPage },
+    ],
+  },
   // {path:"/register",Component:RegisterPage},
 
-   {path:"/forget-password",Component:ForgetPasswordPage},
+  { path: "/forget-password", Component: ForgetPasswordPage },
 
-   {
-      path:"/admin",element:<UserLayout/>,children:[{
-         index:true,Component:UserDashboard
-      }]
-   },
-   {path:"*",element:<ErrorPage code={404} redirectLink="/" redirectTxt="Go Back To Home!!!"/>}
-])
+  {
+    path: "/admin",
+    element: <UserLayout />,
+    children: [
+      {
+        index: true,
+        Component: UserDashboard,
+      },
+
+      { path: "user", element: <UserListPage /> },
+      { path: "user/:userId", element: <UserListPage /> },
+    ],
+  },
+  {
+    path: "*",
+    element: (
+      <ErrorPage code={404} redirectLink="/" redirectTxt="Go Back To Home!!!" />
+    ),
+  },
+]);
 
 export default function RouterConfig() {
-return <>
-<RouterProvider router={router}/>
-</>
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
+
+  
