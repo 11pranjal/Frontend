59d6e14e7c4f3ab1809b6c84b3ea4203eafa3284
Pranjal Thapa <pranjal.thapa555@gmail.com>
2026-02-02 20:33:46 +0545

    react edit delete handling

--- src/router/adminRouter.tsx
+++ src/router/adminRouter.tsx
@@ -4,13 +4,15 @@ import UserListPage from "../pages/user/UserListPage";
 import ErrorPage from "../pages/ErrorPage";
 import BannerListPage from "../pages/banners/BannerListPage";
 import BannerCreatePage from "../pages/banners/BannerCreatePage";
+import BannerEditPage from "../pages/banners/BannerEditPage";
 
 
 export const adminRoutes = [{
     path: "/admin", element: <UserLayout />, children: [
         { index: true, Component: UserDashboard, },
-        {path: "banners", element: <BannerListPage />},
-        {path: "banner/create", element: <BannerCreatePage />},
+        { path: "banners", element: <BannerListPage /> },
+        { path: "banner/create", element: <BannerCreatePage /> },
+        { path: "banner/:id", element: <BannerEditPage />},
         { path: "user", element: <UserListPage /> },
         { path: "user/:userId", element: <UserListPage /> },
         { path: "*", element: (<ErrorPage code={404} redirectLink="/admin" redirectTxt="Go Back To Home!!!" />), },
